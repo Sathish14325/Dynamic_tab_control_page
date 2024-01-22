7b860c19eea107dc450aefa1f488c74f86961c97
Sathish14325 <140421254+Sathish14325@users.noreply.github.com>
2024-01-22 10:26:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let container = document.querySelector(".container");
+    let header = document.querySelector(".header");
+    let menuItem = container.querySelectorAll(".header > div");
+    let tabBody = container.querySelector(".body");
+    let tabBodyElements = container.querySelectorAll(".body > div");
+    let indicator = container.querySelector(".indicator > div");
+  
+    menuItem.forEach((tab, index) => {
+      tab.addEventListener("click", function () {
+        header.querySelector(".active").classList.remove("active");
+        tab.classList.add("active");
+        indicator.style.left = `${index * 25}%`;
+  
+        tabBody.querySelector(".active").classList.remove("active");
+        tabBodyElements[index].classList.add("active");
+      });
+    });
+  });
